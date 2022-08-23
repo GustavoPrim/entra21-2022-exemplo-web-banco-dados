47d0ac9430581c2a98cb507f3bdf744b18bb7196
GustavoPrim <gugahprm@gmail.com>
2022-08-22 21:49:42 -0300

    Melhorias nas classes Editar e Cadastrar - Refatoração, adicionado novamente função de editar

--- Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
+++ Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
@@ -30,10 +30,7 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
         {
             var racas = _racaServico.ObterTodos();
 
-            //passar informação do C# para o HTML
-            ViewBag.Racas = racas;
-
-            return View("Index");
+            return View("Index", racas);
         }
 
         [HttpGet("/raca/cadastrar")]
@@ -69,7 +66,7 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
         {
             _racaServico.Apagar(id);
 
-            return RedirectToAction("index");
+            return RedirectToAction("Index");
         }
 
         [HttpGet("/raca/editar")]
@@ -78,16 +75,29 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
             var raca = _racaServico.ObterPorId(id);
             var especies = ObterEspecies();
 
-            ViewBag.Raca = raca;
+            var racaEditarViewModel = new RacaEditarViewModel
+            {
+                Id = raca.Id,
+                Nome = raca.Nome,
+                Especie = raca.Especie
+            };
+
             ViewBag.Especies = especies;
 
-            return View("Editar");
+            return View(racaEditarViewModel);
         }
 
         [HttpPost("/raca/editar")]
         public IActionResult Editar(
             [FromForm] RacaEditarViewModel racaEditarViewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Especies = ObterEspecies();
+
+                return View(racaEditarViewModel);
+            }
+
             _racaServico.Editar(racaEditarViewModel);
 
             return RedirectToAction("Index");
---
+++ Entra21.CSharp.ClinicaVeterinaria/Repositorio/Mapeamentos/RacaMapeamento.cs
@@ -0,0 +1,47 @@
+﻿using Entra21.CSharp.ClinicaVeterinaria.Repositorio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Entra21.CSharp.ClinicaVeterinaria.Repositorio.Mapeamentos
+{
+    // BD First: Criar uma aplicação onde o banco de dados existente
+    // Code First: Criar um banco de dados apartir de uma aplicação existente
+    // Seed: alimentar o banco de dados com registros
+    // Migration: representação do mapeamento que será aplicado no banco de dados
+    // Mapameamento: representação da entidade em tabelas, colunas, indices...
+    internal class RacaMapeamento : IEntityTypeConfiguration<Raca>
+    {
+        public void Configure(EntityTypeBuilder<Raca> builder)
+        {
+            builder.ToTable("racas");
+
+            builder.HasKey(x => x.Id).HasName("id");
+
+            builder.Property(x => x.Especie)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(100)
+                .IsRequired() // NOT NULL
+                .HasColumnName("especie");
+
+            builder.Property(x => x.Nome)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(30)
+                .IsRequired()
+                .HasColumnName("nome");
+
+            builder.HasData(
+            new Raca
+            {
+                Id = 1,
+                Nome = "Frajola",
+                Especie = "Gato"
+            },
+            new Raca
+            {
+                Id = 2,
+                Nome = "Piupiu",
+                Especie = "Capivara"
+            });
+        }
+    }
+}
